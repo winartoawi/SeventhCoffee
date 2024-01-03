d8f9a51e6b010d7a14a5af21b9430895db093260
winartoawi <Winartoawi@gmail.com>
2024-01-04 02:30:09 +0700

    Fix js

--- src/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("alpine:init", () => {
-  Alpine.data("products", () => ({
-    items: [
-      { id: 1, name: "Hibrido De Timor Beans", img: "1.jpg", price: 20 },
-      { id: 2, name: "USDA", img: "2.jpg", price: 30 },
-    ],
-  }));
-});
